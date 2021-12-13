a18b8c5be4ba0656785f17d0d16ef091f763b1d4
Manav <manavmalviya0309@gmail.com>
2021-12-13 03:54:46 -0500

    putting linear regression in main

--- linear_regression_model.py
+++ linear_regression_model.py
@@ -1,57 +1,47 @@
 """
 linear regression
 """
-
-import pandas as pd
-import plotly.express as px
 from data_cleanup import *
 
 
-# DATAFRAME FOR CONTAINERS FROM SHANGHAI TO TORONTO
-west_dwell = pd.read_csv('data/supply_chain_data.csv')
+def clean_up_end_to_end_df(data: DataFrame) -> None:
+    """
+    Cleans up the supply chain dataframe to get data about transportation time of containers
+    from Shanghai to Toronto via West Coast Ports
+    """
+    # filters rows for Containers, end-to-end Shanghai-Toronto via West Coast ports
+    keep_for_value(data, {'Performance indicators': 'Containers, end-to-end Shanghai-Toronto via West Coast ports'})
 
-# filters rows for Containers, end-to-end Shanghai-Toronto via West Coast ports
-keep_for_value(west_dwell, {'Performance indicators': 'Containers, end-to-end Shanghai-Toronto via West Coast ports'})
+    # removes columns not needed
+    remove_columns(data, [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14])
 
-# removes columns not needed
-remove_columns(west_dwell, [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14])
+    # converts all the dates to the datetime format
+    dates_to_datetime(data, 'REF_DATE')
 
-# converts all the dates to the datetime format
-dates_to_datetime(west_dwell, 'REF_DATE')
+    # removes rows with dates before the pandemic
+    remove_dates_before(data, 'REF_DATE', datetime.date(2020, 2, 1))
 
-# removes rows with dates before the pandemic
-remove_dates_before(west_dwell, 'REF_DATE', datetime.date(2020, 2, 1))
-
-# renames the column
-west_dwell.rename(columns={'VALUE': 'End-to-End Duration Shanghai-Toronto via West Coast ports in Days'}, inplace=True)
+    # renames the column
+    data.rename(columns={'VALUE': 'End-to-End Duration Shanghai-Toronto via West Coast ports in Days'}, inplace=True)
 
 
-# DATAFRAME FOR DEATHS CAUSED BY COVID
-covid_data = pd.read_csv('data/WHO-COVID-19-global-data.csv')
+def clean_covid_for_cases_df(data: DataFrame) -> None:
+    """
+    Clean up the covid dataframe to only have dates and cumulative count of covid cases.
+    """
+    # filters rows for Canadian data only
+    keep_for_value(data, {'Country': 'Canada'})
 
-# filters rows for Canadian data only
-keep_for_value(covid_data, {'Country': 'Canada'})
+    # converts all the dates to the datetime format
+    dates_to_datetime(data, 'Date_reported')
 
-# converts all the dates to the datetime format
-dates_to_datetime(covid_data, 'Date_reported')
+    # renames the column
+    data.rename(columns={'Date_reported': 'REF_DATE', 'Cumulative_cases': 'COVID-19 cases'}, inplace=True)
 
-# renames the column
-covid_data.rename(columns={'Date_reported': 'REF_DATE', 'Cumulative_cases': 'COVID-19 cases'}, inplace=True)
+    # removes columns not needed
+    remove_columns(data, [1, 2, 3, 4, 6, 7])
 
-# removes columns not needed
-remove_columns(covid_data, [1, 2, 3, 4, 6, 7])
-
-# for loop to keep data of only the first day of each month
-for index in covid_data.index:
-    if covid_data['REF_DATE'][index].day != 1:
-        covid_data.drop(index, inplace=True)
-
-# removes dates after november 2020 to be consistent with supply_chain_data.csv
-remove_dates_after(covid_data, 'REF_DATE', datetime.date(2020, 11, 1))
-
-# merging the supply chain data frame with the covid dataframe
-df1 = pd.merge(west_dwell, covid_data, on='REF_DATE')
-
-# Scatter Plot with Linear Regression line to show trend between transportation times and covid cases
-fig1 = px.scatter(df1, y="End-to-End Duration Shanghai-Toronto via West Coast ports in Days", x="COVID-19 cases", trendline='ols')
-fig1.show()
+    # for loop to keep data of only the first day of each month
+    for index in data.index:
+        if data['REF_DATE'][index].day != 1:
+            data.drop(index, inplace=True)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 Enter text here
 """
-import pandas
+import pandas as pd
+import plotly.express as px
 from data_cleanup import remove_columns, filter_for_value
 from categorize_products import *
+from linear_regression_model import *
 import matplotlib
 
 food_data = pandas.read_csv('data/food_data.csv')
@@ -15,3 +17,19 @@ avg_inflation_list = get_average_inflation(foods_lst)
 df = create_inflation_datafram(foods[0].date_list, avg_inflation_list)
 
 df.plot.line(x="DATE")
+
+e2e_data = pd.read_csv('data/supply_chain_data.csv')
+clean_up_end_to_end_df(e2e_data)
+covid_data = pd.read_csv('data/WHO-COVID-19-global-data.csv')
+clean_covid_for_cases_df(covid_data)
+
+# removes dates in covid_data to be consistent with e2e_data
+remove_dates_after(covid_data, 'REF_DATE', datetime.date(2020, 11, 1))
+
+# merging the supply chain data frame with the covid dataframe
+df1 = pd.merge(e2e_data, covid_data, on='REF_DATE')
+
+# Scatter Plot with Linear Regression line to show trend between transportation times and covid cases
+fig1 = px.scatter(df1, y="End-to-End Duration Shanghai-Toronto via West Coast ports in Days", x="COVID-19 cases",
+                  trendline='ols')
+fig1.show()
